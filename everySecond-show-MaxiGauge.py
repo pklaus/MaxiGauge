1751bb537885f0eeb825846e51f7a7b49da252d7
Philipp Klaus <philipp.klaus@gmail.com>
2012-10-25 22:54:49 +0200

    better and more useful example code

---
+++ everySecond-show-MaxiGauge.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+### Load the module:
+from PfeifferVacuum import MaxiGauge, MaxiGaugeError
+import time
+import sys
+
+### Initialize an instance of the MaxiGauge controller with
+### the handle of the serial terminal it is connected to
+mg = MaxiGauge('/dev/ttyUSB1')
+
+### Read out the pressure gauges
+while True:
+    startTime = time.time()
+
+    try:
+        ps = mg.pressures()
+    except MaxiGaugeError, mge:
+        print mge
+        continue
+    line = ""
+    for sensor in ps:
+        #print sensor values
+        if sensor.status in [0,1,2]: # if normal, over or under range
+            line += str(sensor.pressure)
+        line += ", "
+    print line[0:-2] # omit the last comma and space
+    sys.stdout.flush()
+    
+    # do this every second
+    endTime = time.time()-startTime
+    time.sleep(1.0-endTime)
---
+++ store-MaxiGauge.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+### Load the module:
+from PfeifferVacuum import MaxiGauge, MaxiGaugeError
+import time
+import sys
+
+### Initialize an instance of the MaxiGauge controller with
+### the handle of the serial terminal it is connected to
+mg = MaxiGauge('/dev/ttyUSB1')
+logfile = file('measurement-data.txt', 'a')
+
+### Read out the pressure gauges
+while True:
+    startTime = time.time()
+
+    try:
+        ps = mg.pressures()
+    except MaxiGaugeError, mge:
+        print mge
+        continue
+    line = ""
+    for sensor in ps:
+        #print sensor
+        if sensor.status in [0,1,2]:
+            line += "%.3E" % sensor.pressure
+        line += ", "
+    print line[0:-2] # omit the last comma and space
+    sys.stdout.flush()
+    logfile.write(line+'\n')
+    logfile.flush()
+
+    # do this every second
+    endTime = time.time()-startTime
+    time.sleep(1.0-endTime)
