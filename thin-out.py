ab43368bd43fb550ba1937a6088f3d6c93cb02f5
Philipp Klaus <philipp.klaus@gmail.com>
2012-11-19 14:52:25 +0000

    put imports at the places where they are needed

--- thin-out.py
+++ thin-out.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python2
 
 import argparse
-import sys
-import csv
-
 parser = argparse.ArgumentParser(description='Thin out data files')
 parser.add_argument("-n", help="aggregate data and print every nth line", type=int, default=30)
 parser.add_argument("filename", help="File to thin out")
-
 args = parser.parse_args()
 
 try:
     f = open(args.filename)
 except:
+    import sys
     sys.exit(1)
 
+import csv
 log = csv.reader(f)
 
 # Get the 1st line, assuming it contains the column titles
