dc11f3746113d8c1d91c7349ffc60d0041aff08a
Philipp Klaus <philipp.klaus@gmail.com>
2012-11-19 14:51:04 +0000

    explicit file I/O -> csv module

--- thin-out.py
+++ thin-out.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python2
 
 import argparse
-
 import sys
+import csv
 
 parser = argparse.ArgumentParser(description='Thin out data files')
 parser.add_argument("-n", help="aggregate data and print every nth line", type=int, default=30)
@@ -15,9 +15,15 @@ try:
 except:
     sys.exit(1)
 
-print f.readline().strip()
+log = csv.reader(f)
+
+# Get the 1st line, assuming it contains the column titles
+fieldnames = log.next()
+print ','.join(fieldnames)
+
 i = 0
-for line in f:
+for row in log:
+    if len(row) != len(fieldnames): continue
     if i % args.n == 0:
-        print line.strip()
+        print ','.join(row)
     i += 1
